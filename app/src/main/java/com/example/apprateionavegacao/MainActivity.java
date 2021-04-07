01501a15ca2858c459bb3a01899371d3b9206cb6
unknown <jorgeeduardovrb@gmail.com>
2021-04-06 22:39:55 -0300

    adicionando projeto

--- app/src/main/java/com/example/apprateionavegacao/MainActivity.java
+++ app/src/main/java/com/example/apprateionavegacao/MainActivity.java
@@ -1,14 +1,102 @@
 package com.example.apprateionavegacao;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.esri.arcgisruntime.ArcGISRuntimeEnvironment;
+import com.esri.arcgisruntime.mapping.BasemapStyle;;
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.LocationDisplay;
+import com.esri.arcgisruntime.mapping.view.MapView;
 
 public class MainActivity extends AppCompatActivity {
+    private MapView mMapView;
+    private LocationDisplay mLocationDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMapView = findViewById(R.id.mapView);
+        setupMap();
+        setupLocationDisplay();
+        setupGPS();
+    }
+
+    // Criar a função; depois incluir sua chamada no oncreate
+    private void setupLocationDisplay() {
+        mLocationDisplay = mMapView.getLocationDisplay();
+        mLocationDisplay.setAutoPanMode(LocationDisplay.AutoPanMode.COMPASS_NAVIGATION);
+        mLocationDisplay.startAsync();
+    }
+
+    private void setupMap() {
+        if (mMapView != null) {
+            Basemap.Type basemapType = Basemap.Type.STREETS_VECTOR;
+            double latitude = -21.2526;
+            double longitude = -43.1511;
+            int levelOfDetail = 20;
+            ArcGISMap map = new ArcGISMap(basemapType,latitude,longitude,levelOfDetail);
+            mMapView.setMap(map);
+        }
+    }
+
+    private void setupGPS(){
+      // Listen to changes in the status of the location data source.
+        mLocationDisplay.addDataSourceStatusChangedListener(dataSourceStatusChangedEvent-> {
+            if (dataSourceStatusChangedEvent.isStarted() || dataSourceStatusChangedEvent.getError()== null) {
+                return;
+            }
+            int requestPermissionsCode = 2;
+            String[] requestPermissions = new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION};
+            if (!(ContextCompat.checkSelfPermission(MainActivity.this, requestPermissions[0]) ==
+                    PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(MainActivity.this,
+                    requestPermissions[1]) == PackageManager.PERMISSION_GRANTED)) {
+                ActivityCompat.requestPermissions(MainActivity.this, requestPermissions,
+                        requestPermissionsCode);
+            } else {
+                Toast.makeText(MainActivity.this, "Erro.", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            mLocationDisplay.startAsync();
+        } else {
+            Toast.makeText(MainActivity.this, "permissão recusada", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        if (mMapView != null) {
+            mMapView.pause();
+        }
+        super.onPause();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mMapView != null) {
+            mMapView.resume();
+        }
+    }
+    @Override
+    protected void onDestroy() {
+        if (mMapView != null) {
+            mMapView.dispose();
+        }
+        super.onDestroy();
     }
 }
